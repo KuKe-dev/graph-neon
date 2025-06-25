759e699b7867d970185f6f553b844fafc1c4797f
KuKe-dev <lucadibene08@gmail.com>
2025-06-25 20:18:30 -0300

    design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import Graph from "../components/graph/graph";
 
 export default function Home() {
+
+    const [latexValue, setLatexValue] = useState("");
+    const [functionValue, setFunctionValue] = useState("");
+
     return (
         <>
             <Graph />
